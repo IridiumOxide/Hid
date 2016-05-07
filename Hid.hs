ebb93a41a99819578765f5c5544913fb3d8c6634
Filip Chmielewski <filip.chmielewski0@gmail.com>
2016-05-07 22:39:59 +0200

    Rewritten main using new monad, created basic expression evaluating and loc, store, etc. Fixed a print bug in grammar

---
+++ Hid.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import LexHid
+import ParHid
+import AbsHid
+import Interpreter
+
+import Control.Monad
+import Control.Monad.Except
+import Control.Monad.State
+import Control.Monad.Writer
+import ErrM
+
+main = do
+  s <- getContents
+  putStrLn (interpret s)
+  return ()
+
+interpret :: String -> String
+interpret s = let Ok e = pProgram (myLexer s) in let ((eith,l),_) = runState (runWriterT (runExceptT (transProgram e))) emptyMyState in
+    (foldl (\acc x-> acc ++ "\n" ++ x) "" l) ++ case eith of
+        Right () -> "\nProgram finished with no errors"
+        Left s -> "\n------\nERROR:\n" ++ s
